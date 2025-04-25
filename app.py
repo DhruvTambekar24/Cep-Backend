0a16792461f8c40ab089e930c88c4f2bd933ceea
DhruvTambekar24 <d.tambekar24@gmail.com>
2025-04-25 21:24:49 +0530

    Updated model

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def extract_data():
     """
 
     completion = client.chat.completions.create(
-        model="llama-3.2-90b-vision-preview",
+        model="meta-llama/llama-4-scout-17b-16e-instruct",
         messages=[
             {
                 "role": "user",
